4876a75587a02f3e2f643935db5a6e9dafcb12e5
Sakhawat0pu <s19hossain98@gmail.com>
2023-01-06 12:02:55 -0500

    get_item, set_item, insert_item and remove_item methods were added to LL

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -43,7 +43,7 @@ class Linked_list:
         else:
             self._in(node._next, item)
             
-    def add_first(self, item):
+    def add_first(self, item):                      # Prepend
         new_node = Node(item, self._head)
         if len(self) == 0:
             self._head = self._tail = new_node
@@ -51,7 +51,7 @@ class Linked_list:
             self._head = new_node
         self._length += 1
     
-    def add_last(self, item):
+    def add_last(self, item):                      # Append
         if len(self) == 0:
             self.add_first(item)
         else:
@@ -74,7 +74,7 @@ class Linked_list:
             self._length -= 1
             return item
         
-    def remove_last(self):
+    def remove_last(self):                      # Remove last
         if len(self) == 0:
             raise RuntimeError("Cannot remove from an empty collection!!")
         else:
@@ -89,8 +89,58 @@ class Linked_list:
                 self._tail._next = None
                 self._length -= 1
                 return item
-
-
+    
+    def get_item(self, index):
+        if (index < 0) or (index >= len(self)):
+            raise IndexError("Index out of range!!")
+        else:
+            temp = self._head
+            for _ in range(index):
+                temp = temp._next
+            return temp._item
+    
+    def set_item(self, index, item):
+        if (index < 0) or (index >= len(self)):
+            raise IndexError("Index out of range!!")
+        else:
+            temp = self._head
+            for _ in range(index):
+                temp = temp._next
+            temp._item = item
+            
+    def insert_item(self, index, item):
+        if (index < 0) or (index >= len(self)):
+            raise IndexError("Index out of range!!")
+        elif (index == 0):
+            self.add_first(item)
+        elif (index == len(self)):
+            self.add_last(item)
+        else:
+            temp = self._head
+            for _ in range(index-1):
+                temp = temp._next
+            new_node = Node(item, temp._next)
+            temp._next = new_node
+            self._length += 1
+    
+    def remove_item(self, index):
+        if (index < 0) or (index >= len(self)):
+            raise IndexError("Index out of range!!")
+        elif (index == 0):
+            self.remove_first()
+        elif (index == len(self)):
+            self.remove_last()
+        else:
+            temp = self._head
+            for _ in range(index-1):
+                temp = temp._next
+            pre = temp._next
+            item = pre._item
+            temp._next = pre._next
+            pre._next = None
+            self._length -= 1
+            return item
+        
 if __name__ == "__main__":
     n = 10
     if len(sys.argv):
@@ -117,3 +167,15 @@ if __name__ == "__main__":
     print()
     print("Queue is also working...")
     
+    print()
+    for i in range(n):
+        ll.add_last(i)
+    print(ll)
+    print()
+    ll.set_item(5, 100)
+    print(ll)
+    print(ll.get_item(5))
+    
+    ll.insert_item(6, 101)
+    print(ll)
+    print(ll.remove_item(6))
